a6cee3d09438a96cefbd08b815f7e9da372132b5
Edd Tsao <etphonehomies@gmail.com>
2017-01-11 00:50:56 -0800

    Add body parsing

--- index.js
+++ index.js
@@ -3,12 +3,14 @@
 
 const Express = require('express')
 const Morgan = require('morgan')
+const bodyParser = require('body-parser')
 
 const conf = require('config')
 
 const app = Express()
 
 app.use(Morgan(conf.get('logging.format')))
+app.use(bodyParser)
 
 app.listen(conf.get('server.port'), () => {
   console.log(`Server listening on port ${conf.get('server.port')}`)
